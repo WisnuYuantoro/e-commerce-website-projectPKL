c66b1e46d91090bc5b768d419c8bdb4695f6604d
Wisnu Yuantoro <wisnuyuantoro04@gmail.com>
2026-04-27 09:38:39 +0700

    fix: memperbaiki error duplikasi nama ProductController di routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
